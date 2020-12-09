4d532ba094fda5d56952a66dfd1a0212eeaab26b
butterbroad <pyatochevskiy@gmail.com>
2020-12-09 12:51:41 +0300

    finished project

--- #src/js/script.js
+++ #src/js/script.js
@@ -33,7 +33,33 @@ regLinks.forEach(link => {
 });
 
 
+//modal 
+const modalTrigger = document.querySelector('.hero__info');
+const modal = document.querySelector('.modal');
 
+function toggleModal() {
+  modal.classList.toggle('active');
+
+  const modalClose = document.querySelector('.modal-info__close');
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('active');
+  });
+}
+
+modalTrigger.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleModal();
+});
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let isModal = target == modal;
+  let isTrigger = target == modalTrigger;
+  let menuIsActive = modal.classList.contains('active');
+  if (isModal && !isTrigger && menuIsActive) {
+    toggleModal();
+  }
+});
 
 
 
